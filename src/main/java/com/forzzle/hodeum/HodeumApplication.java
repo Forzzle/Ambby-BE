668875456b1b4c78dbcef4e442b9c657fbaebc3b
FhRh <yiyop@naver.com>
2025-04-05 18:58:47 +0900

    init

---
+++ src/main/java/com/forzzle/hodeum/HodeumApplication.java
@@ -0,0 +1,13 @@
+package com.forzzle.hodeum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HodeumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HodeumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/forzzle/hodeum/HodeumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forzzle.hodeum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HodeumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
